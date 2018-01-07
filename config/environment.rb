84ee19ce6d9bdcbdc12e7eb900c2a2f7466b8859
hollyjoc <holly.jo.robertson22@gmail.com>
2018-01-07 14:30:03 -0500

    Syntax errors in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
 	:port => '587', 
 	:authentication => :plain, 
 	:user_name => ENV['SENDGRID_USERNAME']
-	:password => ENV['SENDGRID_PASSWORD']
-	:domain => 'heroku.com'
+	:password => ENV['SENDGRID_PASSWORD'],
+	:domain => 'heroku.com',
 	:enable_startstls_auto => true
 
 }
